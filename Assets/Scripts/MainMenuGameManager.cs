24c1f372c23babd2007e83e4118d0135e6da969f
Hrishikesh-Panigrahi <hrishikesh.panigrahi38@gmail.com>
2022-10-05 08:35:14 +0530

    1st

---
+++ Assets/Scripts/MainMenuGameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MainMenuGameManager : MonoBehaviour
+{
+
+
+ public void Scene1()
+    {
+        SceneManager.LoadScene("SampleScene"); 
+         
+    }
+
+     
+//  public void Scene2()
+//     {
+//         SceneManager.LoadScene("SampleScene"); 
+         
+//     }  
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
