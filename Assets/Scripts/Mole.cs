ceee5daee9e9b8c0c24a2d169f56ffec9efcc642
KaushikBedmutha <85838410+KaushikBedmutha@users.noreply.github.com>
2023-05-01 23:25:03 +0530

    Update Mole.cs

--- Assets/Scripts/Mole.cs
+++ Assets/Scripts/Mole.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+// Comment 
 public class Mole : MonoBehaviour
 {
  [Header("Graphics")]
